35b66b49cc9f4006c2359b308b1cd4c86fbc0fb7
david <davidhunt@mitre.org>
2019-06-15 08:02:32 -0400

    reworking the gui

--- hook.py
+++ hook.py
@@ -2,7 +2,7 @@ from plugins.gui.app.gui_api import GuiApi
 
 name = 'GUI'
 description = 'Provides a web application structure & authentication for other plugins to build upon'
-address = '/'
+address = None
 
 
 async def initialize(app, services):
