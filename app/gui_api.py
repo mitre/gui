2d2ddcf22f106d07774bffe3023fa6c6b7aa80b0
Henry Foster <hfoster@mitre.org>
2019-07-01 12:37:26 -0400

    add att&ck link and support custom att&ck url

--- app/gui_api.py
+++ app/gui_api.py
@@ -3,9 +3,19 @@ from aiohttp_jinja2 import template
 
 class GuiApi:
 
-    def __init__(self, auth_svc, plugins):
-        self.auth_svc = auth_svc
+    def __init__(self, services, plugins):
+        self.services = services
+        self.caldera_config = services.get('utility_svc').get_config()
+        self.auth_svc = services.get('auth_svc')
         self.plugins = plugins
+
+    def get_attack_url(self, request=None):
+        if 'attack_svc' in self.services:
+            return self.services.get('attack_svc').get_attack_url(request=request)
+        elif 'attack_url' in self.caldera_config:
+            return self.caldera_config['attack_url']
+        else:
+            return 'https://attack.mitre.org'
 
     @template('login.html')
     async def login(self, request):
@@ -23,5 +33,5 @@ class GuiApi:
         await self.auth_svc.check_permissions(request)
         p = [dict(name=getattr(p, 'name'), description=getattr(p, 'description'), address=getattr(p, 'address'))
              for p in self.plugins]
-        return dict(plugins=p)
+        return dict(plugins=p, attack_url=self.get_attack_url(request=request))
 
--- hook.py
+++ hook.py
@@ -6,7 +6,8 @@ address = None
 
 
 async def initialize(app, services):
-    gui_api = GuiApi(auth_svc=services.get('auth_svc'), plugins=services.get('plugins'))
+    gui_api = GuiApi(services=services,
+                     plugins=services.get('plugins'))
     app.router.add_static('/gui', 'plugins/gui/static/', append_version=True)
     app.router.add_route('*', '/', gui_api.home)
     app.router.add_route('*', '/enter', gui_api.validate_login)
